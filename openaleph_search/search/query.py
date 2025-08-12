ececffb01ba4c00360bdf02773942c379594e43b
Simon Wörpel <simon.woerpel@pm.me>
2025-08-12 16:25:22 +0200

    🔊 (search) Log debugging info

--- openaleph_search/search/query.py
+++ openaleph_search/search/query.py
@@ -292,16 +292,17 @@ class Query(object):
 
     def search(self):
         """Execute the query as assmbled."""
-        # log.info("Search index: %s", self.get_index())
+        log.debug("Search index: %s", self.get_index())
         es = get_es()
         result = es.search(index=self.get_index(), body=self.get_body())
-        # log.info(
-        #     f"Elasticsearch query [{self.to_text()}] took {result.get('took')}ms",
-        #     query=self.to_text(),
-        #     took=result.get("took"),
-        # )
-        # log.info("%s", pformat(self.get_body()))
-        # log.info("%s", pformat(self.parser.filters))
+        log.debug(
+            f"Elasticsearch query [{self.to_text()}] took {result.get('took')}ms",
+            query=self.to_text(),
+            body=self.get_body(),
+            filters=self.parser.filters,
+            took=result.get("took"),
+            hits=result.get("hits", {}).get("total", {}).get("value"),
+        )
         return result
 
     @classmethod
