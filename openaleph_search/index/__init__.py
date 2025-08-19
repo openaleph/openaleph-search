cc81507490a55dd2ddccf56e844cd242533159dc
Simon Wörpel <simon.woerpel@pm.me>
2025-08-19 21:42:02 +0200

    🚧 Add index exports

---
+++ openaleph_search/index/__init__.py
@@ -0,0 +1,4 @@
+from openaleph_search.index.entities import entities_read_index, entities_write_index
+from openaleph_search.index.xref import xref_index
+
+__all__ = ["entities_read_index", "entities_write_index", "xref_index"]
