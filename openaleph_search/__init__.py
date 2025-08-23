0533ccc92601649ad6e5b8bb236de0c292c11cfe
Simon Wörpel <simon.woerpel@pm.me>
2025-08-23 15:18:24 +0200

    🚧 main export QueryResult

--- openaleph_search/__init__.py
+++ openaleph_search/__init__.py
@@ -1,10 +1,6 @@
 from openaleph_search.parse.parser import QueryParser, SearchQueryParser
 from openaleph_search.query.base import Query
-from openaleph_search.query.queries import (
-    EntitiesQuery,
-    GeoDistanceQuery,
-    MatchQuery,
-)
+from openaleph_search.query.queries import EntitiesQuery, GeoDistanceQuery, MatchQuery
 from openaleph_search.search.result import QueryResult
 
 __all__ = [
