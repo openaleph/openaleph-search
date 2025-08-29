22f0e1611d6f8d133ab6101c9fd66981c08e1167
Simon Wörpel <simon.woerpel@pm.me>
2025-08-29 19:04:40 +0200

    🚧 Export MoreLikeThisQuery

--- openaleph_search/__init__.py
+++ openaleph_search/__init__.py
@@ -1,12 +1,18 @@
 from openaleph_search.index.util import unpack_result
 from openaleph_search.parse.parser import QueryParser, SearchQueryParser
 from openaleph_search.query.base import Query
-from openaleph_search.query.queries import EntitiesQuery, GeoDistanceQuery, MatchQuery
+from openaleph_search.query.queries import (
+    EntitiesQuery,
+    GeoDistanceQuery,
+    MatchQuery,
+    MoreLikeThisQuery,
+)
 
 __all__ = [
     "EntitiesQuery",
     "GeoDistanceQuery",
     "MatchQuery",
+    "MoreLikeThisQuery",
     "Query",
     "QueryParser",
     "SearchQueryParser",
