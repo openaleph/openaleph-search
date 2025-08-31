dd272dfc4dd78b5aee86605a52d1bcca9e880809
Simon Wörpel <simon.woerpel@pm.me>
2025-08-31 11:32:57 +0200

    🔖 Bump version: 0.0.6 → 0.0.7

--- openaleph_search/settings.py
+++ openaleph_search/settings.py
@@ -2,7 +2,7 @@ from anystore.settings import BaseSettings
 from pydantic import AliasChoices, Field, HttpUrl
 from pydantic_settings import SettingsConfigDict
 
-__version__ = "0.0.6"
+__version__ = "0.0.7"
 
 MAX_PAGE = 9999
 BULK_PAGE = 1000
